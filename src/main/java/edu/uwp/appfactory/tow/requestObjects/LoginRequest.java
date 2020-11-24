fa78d0cca900c24fd3e53d6e1c271bb77a04226a
Quincy Kayle <kayle001@rangers.uwp.edu>
2020-11-24 12:37:17 -0600

    fixed login request

--- src/main/java/edu/uwp/appfactory/tow/requestObjects/LoginRequest.java
+++ src/main/java/edu/uwp/appfactory/tow/requestObjects/LoginRequest.java
@@ -1,6 +1,18 @@
 package edu.uwp.appfactory.tow.requestObjects;
 
-public interface LoginRequest {
-    String getEmail();
-    String getPassword();
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+
+public class LoginRequest {
+    @NotBlank
+    @Getter
+    @Setter
+    private String email;
+
+    @NotBlank
+    @Getter
+    @Setter
+    private String password;
 }
