2ca6930d29a94d5acdf888bcf07b792d0516f0b6
Gianluca Eickenberg <eickenbg@uwp.edu>
2020-12-06 23:22:28 -0600

    return 204

--- src/main/java/edu/uwp/appfactory/tow/routes/LocationRoutes.java
+++ src/main/java/edu/uwp/appfactory/tow/routes/LocationRoutes.java
@@ -5,6 +5,7 @@ import edu.uwp.appfactory.tow.WebSecurityConfig.security.jwt.JwtUtils;
 import edu.uwp.appfactory.tow.controllers.LocationController;
 import edu.uwp.appfactory.tow.requestObjects.DriversRequest;
 import edu.uwp.appfactory.tow.requestObjects.SetLocationRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -31,7 +32,7 @@ public class LocationRoutes {
         String userUUID = jwtUtils.getUUIDFromJwtToken(jwtToken);
         System.out.println("lat: " + setRequest.getLatitude() + " long: " + setRequest.getLongitude() + " active: " + setRequest.isActive());
         return locationController.setLocation(setRequest.getLatitude(), setRequest.getLongitude(), setRequest.isActive(), userUUID)
-                ? ResponseEntity.ok().body(new MessageResponse("Success"))
+                ? ResponseEntity.status(HttpStatus.NO_CONTENT).body(null)
                 : ResponseEntity.status(400).body("Error");
     }
 
@@ -39,9 +40,7 @@ public class LocationRoutes {
     //todo: it needs to be 200 or something even if no drivers are returned
     @PreAuthorize("hasRole('DISPATCHER')")
     @PatchMapping("/driver-locations")
-    public ResponseEntity<?> getLocations(@RequestHeader("Authorization") final String jwtToken,
-                                          @RequestBody DriversRequest driversRequest) {
-        String userUUID = jwtUtils.getUUIDFromJwtToken(jwtToken);
+    public ResponseEntity<?> getLocations(@RequestBody DriversRequest driversRequest) {
         List<?> data = locationController.findByDistance(driversRequest.getLatitude(), driversRequest.getLongitude(), driversRequest.getRadius());
         if (data != null) {
             return ResponseEntity.ok(data);
