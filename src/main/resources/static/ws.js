0678484701d8ba0bfd496cc24e468b888cfc04bd
Quincy K <kayleqb@gmail.com>
2021-02-24 13:49:02 -0600

    Comment'd out websocket, prep for staging

--- src/main/resources/static/ws.js
+++ src/main/resources/static/ws.js
@@ -1,26 +1,26 @@
-let stompClient = null;
-
-var vm = new Vue({
-    el: "#app",
-    mounted: function() {
-        this.$nextTick(function() {
-            let socket = new SockJS("/api/tow-ws");
-            stompClient = Stomp.over(socket);
-            stompClient.connect(
-                {},
-                function(frame) {
-                    console.log("Connected: " + frame);
-
-                    stompClient.subscribe("/info/values", function(val) {
-                        vm.valuesList.push(JSON.parse(val.body));
-                    });
-                }
-            );
-        });
-    },
-    data: function() {
-        return {
-            valuesList: []
-        };
-    }
-});
+// let stompClient = null;
+//
+// var vm = new Vue({
+//     el: "#app",
+//     mounted: function() {
+//         this.$nextTick(function() {
+//             let socket = new SockJS("/api/tow-ws");
+//             stompClient = Stomp.over(socket);
+//             stompClient.connect(
+//                 {},
+//                 function(frame) {
+//                     console.log("Connected: " + frame);
+//
+//                     stompClient.subscribe("/info/values", function(val) {
+//                         vm.valuesList.push(JSON.parse(val.body));
+//                     });
+//                 }
+//             );
+//         });
+//     },
+//     data: function() {
+//         return {
+//             valuesList: []
+//         };
+//     }
+// });
