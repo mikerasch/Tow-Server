04ace74075e2864648eaebe43ca7f5caf65e2367
Gianluca <gianluca.eickenberg@gmail.com>
2021-02-27 13:40:09 -0600

    fixed null pointer

--- src/main/java/edu/uwp/appfactory/tow/entities/File.java
+++ src/main/java/edu/uwp/appfactory/tow/entities/File.java
@@ -6,11 +6,13 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.Persistent;
+import org.springframework.data.relational.core.mapping.Table;
 
 import java.util.UUID;
 
 @Data
 @Builder
+@Table("file_db")
 @NoArgsConstructor
 @AllArgsConstructor
 public class File {
