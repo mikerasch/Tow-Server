4675a01fa2087a8b8d03915555d6a5f51bca1ad0
Quincy K <kayleqb@gmail.com>
2021-02-28 16:53:30 -0600

    cors error fix

--- src/main/java/edu/uwp/appfactory/tow/websocket/WebSocketSecurityConfig.java
+++ src/main/java/edu/uwp/appfactory/tow/websocket/WebSocketSecurityConfig.java
@@ -5,8 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.SimpMessageType;
 import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
 import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Configuration
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class WebSocketSecurityConfig extends AbstractSecurityWebSocketMessageBrokerConfigurer {
 
     @Override
