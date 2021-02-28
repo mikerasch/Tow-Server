181a57c0b704c9a7b56fd33c9fcafa0e25fc9abd
Quincy K <kayleqb@gmail.com>
2021-02-28 17:12:50 -0600

    cors error fix

--- src/main/java/edu/uwp/appfactory/tow/websocket/WebSocketConfig.java
+++ src/main/java/edu/uwp/appfactory/tow/websocket/WebSocketConfig.java
@@ -72,6 +72,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
     }
 }
