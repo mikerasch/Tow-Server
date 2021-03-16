f009d012107c6542bdea75a865b3b9878e4bc94b
mattrank93 <ChloeRdoe1Bike2021>
2021-03-15 20:30:37 -0500

    Expanded websocket topics to include setlocation and driver connect with string returns for testing

--- src/main/java/edu/uwp/appfactory/tow/stomp/controllers/MessageController.java
+++ src/main/java/edu/uwp/appfactory/tow/stomp/controllers/MessageController.java
@@ -1,15 +1,27 @@
 package edu.uwp.appfactory.tow.stomp.controllers;
 
+import edu.uwp.appfactory.tow.controllers.LocationController;
 import edu.uwp.appfactory.tow.stomp.models.MessageRequest;
 import edu.uwp.appfactory.tow.stomp.models.MessageResponse;
+import edu.uwp.appfactory.tow.webSecurityConfig.security.jwt.JwtUtils;
 import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.util.HtmlUtils;
+import edu.uwp.appfactory.tow.controllers.LocationController;
+import edu.uwp.appfactory.tow.webSecurityConfig.security.jwt.JwtUtils;
 
 @Controller
 public class MessageController {
+    private final LocationController locationController;
+    private final JwtUtils jwtUtils;
+
+    public MessageController(LocationController locationController, JwtUtils jwtUtils) {
+        this.locationController = locationController;
+        this.jwtUtils = jwtUtils;
+    }
 
     @MessageMapping("/guestchat")  // here
     @SendTo("/topic/guestchats")  // here
@@ -22,6 +34,22 @@ public class MessageController {
     @SendTo("/topic/guestupdates")
     public MessageResponse handleUserTyping(MessageRequest message) throws Exception {
         return new MessageResponse("Someone is typing...");
+    }
+
+    @MessageMapping("/driverconnect")
+    @SendTo("/topic/driverconnected")
+    public MessageResponse handleconnectionrequest(MessageRequest message) throws Exception {
+
+        return new MessageResponse("Driver connecting....");
+    }
+
+    @PreAuthorize("hasRole('TCUSER')")
+    @MessageMapping("//my-location")
+    @SendTo("/topic/driverconnected")
+    public MessageResponse setlocation(MessageRequest message) throws Exception {
+
+        System.out.println(message);
+        return new MessageResponse("Setting location....");
     }
 
     @MessageMapping("/guestjoin")
