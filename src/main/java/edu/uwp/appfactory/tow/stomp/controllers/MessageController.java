bef6af4673bb0a40210a8ce8cbd1309da19f3c60
mattrank93 <ChloeRdoe1Bike2021>
2021-03-17 13:02:59 -0500

    Functioning STOMP connect and websocket setlocation function working with role restriction.

--- src/main/java/edu/uwp/appfactory/tow/stomp/controllers/MessageController.java
+++ src/main/java/edu/uwp/appfactory/tow/stomp/controllers/MessageController.java
@@ -1,5 +1,4 @@
 package edu.uwp.appfactory.tow.stomp.controllers;
-
 import edu.uwp.appfactory.tow.controllers.LocationController;
 import edu.uwp.appfactory.tow.requestObjects.TCULocationRequest;
 import edu.uwp.appfactory.tow.stomp.models.MessageRequest;
@@ -11,29 +10,8 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.util.HtmlUtils;
-import edu.uwp.appfactory.tow.controllers.LocationController;
-import edu.uwp.appfactory.tow.webSecurityConfig.security.jwt.JwtUtils;
 
-
-import edu.uwp.appfactory.tow.controllers.LocationController;
-import edu.uwp.appfactory.tow.requestObjects.TCULocationRequest;
-import edu.uwp.appfactory.tow.stomp.models.MessageRequest;
-import edu.uwp.appfactory.tow.stomp.models.MessageResponse;
-import edu.uwp.appfactory.tow.webSecurityConfig.security.jwt.JwtUtils;
-import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.util.HtmlUtils;
-import edu.uwp.appfactory.tow.controllers.LocationController;
-import edu.uwp.appfactory.tow.webSecurityConfig.security.jwt.JwtUtils;
 
 
 
@@ -67,27 +45,19 @@ public class MessageController {
         return new MessageResponse("Driver connecting....");
     }
 
-    //@PreAuthorize("hasRole('TCUSER')")
+    @PreAuthorize("hasRole('PDUSER')")
     @MessageMapping("/my-location")
     @SendTo("/topic/setlocation")
-    public ResponseEntity<?> setlocation(@RequestHeader("Authorization") final String jwtToken,
-                                         @RequestBody TCULocationRequest setRequest) throws Exception {
-        String userUUID = jwtUtils.getUUIDFromJwtToken(jwtToken);
+    public ResponseEntity<?> setlocation(TCULocationRequest setRequest) throws Exception {
+        String userUUID = jwtUtils.getUUIDFromJwtToken(setRequest.getJwtToken());
         System.out.println("lat: " + setRequest.getLatitude() + " long: " + setRequest.getLongitude() + " active: " + setRequest.isActive());
-        return ResponseEntity.status(200).body("setting location");
-//                locationController.setLocation(setRequest.getLatitude(), setRequest.getLongitude(), setRequest.isActive(), userUUID)
-//                ? ResponseEntity.status(204).body(null)
-//                : ResponseEntity.status(400).build();
+        return locationController.setLocation(setRequest.getLatitude(), setRequest.getLongitude(), setRequest.isActive(), userUUID)
+                ? ResponseEntity.status(204).body(null)
+                : ResponseEntity.status(400).build();
     }
 
-    //    public ResponseEntity<?> setLocation(@RequestHeader("Authorization") final String jwtToken,
-//                                         @RequestBody TCULocationRequest setRequest) {
-//        String userUUID = jwtUtils.getUUIDFromJwtToken(jwtToken);
-//        System.out.println("lat: " + setRequest.getLatitude() + " long: " + setRequest.getLongitude() + " active: " + setRequest.isActive());
-//        return locationController.setLocation(setRequest.getLatitude(), setRequest.getLongitude(), setRequest.isActive(), userUUID)
-//                ? ResponseEntity.status(NO_CONTENT).body(null)
-//                : ResponseEntity.status(BAD_REQUEST).build();
-//    }
+
+
     @MessageMapping("/guestjoin")
     @SendTo("/topic/guestnames")
     public MessageResponse handleMemberJoins(String string) throws Exception {
