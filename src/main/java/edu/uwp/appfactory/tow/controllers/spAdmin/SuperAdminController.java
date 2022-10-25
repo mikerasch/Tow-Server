7ad134e597a869972aae9e124eeec0d71bb891ea
Click <raschmichael10@gmail.com>
2022-10-24 21:32:30 -0500

    Removed unnecessary import

--- src/main/java/edu/uwp/appfactory/tow/controllers/spAdmin/SuperAdminController.java
+++ src/main/java/edu/uwp/appfactory/tow/controllers/spAdmin/SuperAdminController.java
@@ -4,7 +4,6 @@ import edu.uwp.appfactory.tow.requestObjects.rolerequest.SuperAdminRequest;
 import edu.uwp.appfactory.tow.responseObjects.TestVerifyResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
