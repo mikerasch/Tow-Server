b5ecc7c83dfbf17db92b9c71b6af47c4606cd683
Click <raschmichael10@gmail.com>
2022-10-24 22:42:09 -0500

    Added documentation for features/validate_information and private constructor

--- src/main/java/edu/uwp/appfactory/tow/utilities/AccountInformationValidator.java
+++ src/main/java/edu/uwp/appfactory/tow/utilities/AccountInformationValidator.java
@@ -5,9 +5,12 @@ import org.passay.*;
 import java.util.Arrays;
 import java.util.regex.Pattern;
 
+/**
+ * Utility class for providing validation of email and password.
+ */
 public class AccountInformationValidator {
-    private static final String emailPattern = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@" + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
-    private static final PasswordValidator validator = new PasswordValidator(Arrays.asList(
+    private static final String EMAIL_PATTERN = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@" + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
+    private static final PasswordValidator VALIDATOR = new PasswordValidator(Arrays.asList(
             new LengthRule(8,30), // password must be 8 <-> 30
             new CharacterRule(EnglishCharacterData.UpperCase,1), // password must contain an uppercase
             new CharacterRule(EnglishCharacterData.Digit,1), // password must contain a digit
@@ -15,14 +18,32 @@ public class AccountInformationValidator {
             new IllegalSequenceRule(EnglishSequenceData.Numerical,5,false), // password must not contain 5 repeating numbers
             new WhitespaceRule() // password must not contain whitespace
     ));
+
+    private AccountInformationValidator(){
+
+    }
+    /**
+     * Validates email based on a strict regex pattern.
+     * @param email - email to be validated
+     * @return true if good email, false otherwise
+     */
     public static boolean validateEmail(String email){
-        return Pattern.compile(emailPattern)
+        if(email.length() > 50){
+            return false;
+        }
+        return Pattern.compile(EMAIL_PATTERN)
                 .matcher(email)
                 .matches();
     }
 
+    /**
+     * Validates password based on a strict rule set.
+     * See validator variable for rule set.
+     * @param password - password to be validated
+     * @return true if good password, false otherwise.
+     */
     public static boolean validatePassword(String password){
-        RuleResult result = validator.validate(new PasswordData(password));
+        RuleResult result = VALIDATOR.validate(new PasswordData(password));
         return result.isValid();
     }
 }
