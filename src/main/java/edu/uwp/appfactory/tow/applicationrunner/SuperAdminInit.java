a7f51ae53255ee1ee0d8a123749d6b585e275a93
Mike <54646452+mikerasch@users.noreply.github.com>
2023-04-21 19:25:55 -0500

    Features/finish todos (#29)
    * JPA changes, bug fixes, test fixes, delete functionality, to-do fixes
    
    * More fixes

--- src/main/java/edu/uwp/appfactory/tow/applicationrunner/SuperAdminInit.java
+++ src/main/java/edu/uwp/appfactory/tow/applicationrunner/SuperAdminInit.java
@@ -2,6 +2,7 @@ package edu.uwp.appfactory.tow.applicationrunner;
 
 import edu.uwp.appfactory.tow.entities.SPAdmin;
 import edu.uwp.appfactory.tow.repositories.SuperAdminRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -11,6 +12,12 @@ import org.springframework.stereotype.Component;
 public class SuperAdminInit implements ApplicationRunner {
     private final SuperAdminRepository superAdminRepository;
     private final PasswordEncoder passwordEncoder;
+    @Value("${SP_EMAIL}")
+    private String email;
+    @Value("${SP_PASSWORD}")
+    private String password;
+    @Value("${SP_PHONE")
+    private String phone;
     public SuperAdminInit(SuperAdminRepository superAdminRepository, PasswordEncoder passwordEncoder) {
         this.superAdminRepository = superAdminRepository;
         this.passwordEncoder = passwordEncoder;
@@ -18,13 +25,13 @@ public class SuperAdminInit implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        if (!superAdminRepository.existsByEmail("spadmin123@gmail.com")) {
+        if (!superAdminRepository.existsByEmail(email)) {
             SPAdmin spAdmin = new SPAdmin(
                     "super admin",
                     "super admin",
-                    "spadmin123@gmail.com",
-                    passwordEncoder.encode("dingledart123A!"),
-                    "555-555-5555",
+                    email,
+                    passwordEncoder.encode(password),
+                    phone,
                     "ROLE_SPADMIN",
                     "spadmin"
             );
