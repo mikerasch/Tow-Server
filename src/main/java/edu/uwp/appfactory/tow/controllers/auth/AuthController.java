566b27bb378a271bd16e2fb37610fd0e7293cde1
Mike <raschmichael10@gmail.com>
2023-04-22 17:19:11 -0500

    Fixed more violations

--- src/main/java/edu/uwp/appfactory/tow/controllers/auth/AuthController.java
+++ src/main/java/edu/uwp/appfactory/tow/controllers/auth/AuthController.java
@@ -21,7 +21,6 @@ import static org.springframework.http.HttpStatus.*;
  * post - /admin
  * get - /verification
  */
-@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
